3c10c3a0e770dbe0e72349bbb67a28cca34213e5
Samarth Kamble <kamble.samarth91@gmail.com>
2024-05-13 16:31:30 +0530

    Connected to neon.tech db using prisma

--- app/configure/layout.tsx
+++ app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
+import Steps from "@/components/shared/Steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
