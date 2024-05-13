ce5b1e4da0922a2884015d50e0c7df5f09ee3e25
Samarth Kamble <kamble.samarth91@gmail.com>
2024-05-13 23:32:20 +0530

    started the designed case design page

--- app/configure/design/page.tsx
+++ app/configure/design/page.tsx
@@ -1,7 +1,36 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 import React from "react";
+import DesignConfigrator from "./DesignConfigrator";
 
-const Page = async () => {
-  return <div>Page</div>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configration) {
+    return notFound();
+  }
+  const { imageUrl, width, height } = configration;
+
+  return (
+    <DesignConfigrator
+      configId={configration.id}
+      imageDimension={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default Page;
