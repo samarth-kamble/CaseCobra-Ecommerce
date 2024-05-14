c7e469d42a575805eb66251c22b7a4383fd7ec11
Samarth Kamble <kamble.samarth91@gmail.com>
2024-05-15 01:05:33 +0530

    Add material and finish options to DesignConfigrator
    This commit adds the material and finish options to the DesignConfigrator component. The options are retrieved from the option-validator module and rendered as radio buttons in the UI. The selected options are stored in the state and used to calculate the total price of the design configuration. This enhancement improves the user experience by allowing users to choose different materials and finishes for their designs.

--- app/configure/design/DesignConfigrator.tsx
+++ app/configure/design/DesignConfigrator.tsx
@@ -3,12 +3,17 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import React, { useState } from "react";
 import NextImage from "next/image";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 import { Rnd } from "react-rnd";
 import HandleComponent from "@/components/shared/HandleComponent";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { RadioGroup } from "@headlessui/react";
-import { COLORS, MODELS } from "@/validator/option-validator";
+import {
+  COLORS,
+  FINISHES,
+  MATERIALS,
+  MODELS,
+} from "@/validator/option-validator";
 import { Label } from "@/components/ui/label";
 import {
   DropdownMenu,
@@ -17,7 +22,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
+import { set } from "zod";
+import { BASE_PRICE } from "@/config/products";
 
 interface DesignConfigratorProps {
   configId: string;
@@ -32,9 +39,13 @@ const DesignConfigrator = ({
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number];
     model: (typeof MODELS.options)[number];
+    material: (typeof MATERIALS.options)[number];
+    finish: (typeof FINISHES.options)[number];
   }>({
     color: COLORS[0],
     model: MODELS.options[0],
+    material: MATERIALS.options[0],
+    finish: FINISHES.options[0],
   });
 
   return (
@@ -174,10 +185,95 @@ const DesignConfigrator = ({
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
+
+                {[MATERIALS, FINISHES].map(
+                  ({ name, options: selectableOptions }) => (
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(val) => {
+                        setOptions((prev) => ({
+                          ...prev,
+                          [name]: val,
+                        }));
+                      }}
+                    >
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      <div className="mt-3 space-y-4">
+                        {selectableOptions.map((option) => (
+                          <RadioGroup.Option
+                            key={option.value}
+                            value={option}
+                            className={({ active, checked }) =>
+                              cn(
+                                "realtive block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
+                                {
+                                  "border-primary": active || checked,
+                                }
+                              )
+                            }
+                          >
+                            <span className="flex items-center">
+                              <span className="flex flex-col text-sm">
+                                <RadioGroup.Label
+                                  as="span"
+                                  className={"font-medium text-gray-900"}
+                                >
+                                  {option.label}
+                                </RadioGroup.Label>
+                                {option.description ? (
+                                  <RadioGroup.Description
+                                    as="span"
+                                    className={"text-gray-500"}
+                                  >
+                                    <span className="block sm:inline">
+                                      {option.description}
+                                    </span>
+                                  </RadioGroup.Description>
+                                ) : null}
+                              </span>
+                            </span>
+
+                            <RadioGroup.Description
+                              as="span"
+                              className={
+                                "mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right"
+                              }
+                            >
+                              <span className="font-medium text-gray-900">
+                                {formatPrice(option.price / 100)}
+                              </span>
+                            </RadioGroup.Description>
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  )
+                )}
               </div>
             </div>
           </div>
         </ScrollArea>
+
+        <div className="w-full px-8 h-16 bg-white">
+          <div className="h-px w-full bg-zinc-200" />
+          <div className="w-full h-full flex justify-end items-center">
+            <div className="w-full flex gap-6 items-center">
+              <p className="font-medium whitespace-nowrap">
+                {formatPrice(
+                  (BASE_PRICE + options.finish.price + options.material.price) /
+                    100
+                )}
+              </p>
+              <Button size={"sm"} className="w-full">
+                Continue
+                <ArrowRight className=" h-4 w-4 ml-1.5 inline" />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- validator/option-validator.ts
+++ validator/option-validator.ts
@@ -61,3 +61,21 @@ export const MATERIALS = {
     },
   ],
 } as const;
+
+export const FINISHES = {
+  name: "finish",
+  options: [
+    {
+      label: "Smooth Finish",
+      value: "smooth",
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: "Textured Finish",
+      value: "textured",
+      description: "Soft grippy texture",
+      price: PRODUCT_PRICES.finish.textured,
+    },
+  ],
+} as const;
